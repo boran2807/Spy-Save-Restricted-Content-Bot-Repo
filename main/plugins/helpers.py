18c79e9ae855ccd43c263b5efe1308757e919368
boran2807 <151364290+boran2807@users.noreply.github.com>
2024-03-07 20:49:21 +0530

    Update helpers.py

--- main/plugins/helpers.py
+++ main/plugins/helpers.py
@@ -1,4 +1,4 @@
-#Github.com/devgaganin
+#Github.com/boran2807
 
 from pyrogram.errors import FloodWait, InviteHashInvalid, InviteHashExpired, UserAlreadyParticipant
 from telethon import errors, events
